fbf8e1203565bb09c77f6b0412412bcc819aaf4d
BambooSword <herodreamdeng@gmail.com>
2023-07-27 23:13:04 +0800

    class-validator/transformer modules

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,42 @@
-import { ProjectInput } from './components/project-input'
-import { ProjectList } from './components/project-list'
-import { ProjectStatus } from './models/project'
+// import _ from 'lodash'
 
-const prjInput = new ProjectInput()
-const activePrjList = new ProjectList(ProjectStatus.Active)
-const finishedPrjList = new ProjectList(ProjectStatus.Finished)
+// declare var GLOBAL: any
+// console.log('====================================')
+// console.log(_.shuffle([1, 2, 3]))
+// console.log(GLOBAL)
+// console.log('====================================')
+
+import { Product } from './product.model'
+import { plainToInstance } from 'class-transformer'
+import { validate } from 'class-validator'
+
+const products = [
+  {
+    title: 'A Carpet',
+    price: 29.99,
+  },
+  {
+    title: 'A Novel',
+    price: 10.99,
+  },
+]
+
+const instances = plainToInstance(Product, products)
+
+const newProd = new Product('', -5.0)
+validate(newProd).then(error => {
+  if (error.length > 0) {
+    console.log('🚀 ~ file: app.ts:27 ~ validate ~ error:', error)
+  } else {
+    console.log(
+      '🚀 ~ file: app.ts:26 ~ ins.getInformation():',
+      newProd.getInformation()
+    )
+  }
+})
+for (const ins of instances) {
+	console.log(
+		'🚀 ~ file: app.ts:26 ~ ins.getInformation():',
+		ins.getInformation()
+	)
+}
