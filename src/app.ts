4cb356e14d491edd4df3c71f787620435d89de43
BambooSword <herodreamdeng@gmail.com>
2023-07-28 11:17:53 +0800

    init: init the project

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,9 @@
-// import _ from 'lodash'
+const form = document.getElementById('form')!
+const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-// declare var GLOBAL: any
-// console.log('====================================')
-// console.log(_.shuffle([1, 2, 3]))
-// console.log(GLOBAL)
-// console.log('====================================')
-
-import { Product } from './product.model'
-import { plainToInstance } from 'class-transformer'
-import { validate } from 'class-validator'
-
-const products = [
-  {
-    title: 'A Carpet',
-    price: 29.99,
-  },
-  {
-    title: 'A Novel',
-    price: 10.99,
-  },
-]
-
-const instances = plainToInstance(Product, products)
-
-const newProd = new Product('', -5.0)
-validate(newProd).then(error => {
-  if (error.length > 0) {
-    console.log('🚀 ~ file: app.ts:27 ~ validate ~ error:', error)
-  } else {
-    console.log(
-      '🚀 ~ file: app.ts:26 ~ ins.getInformation():',
-      newProd.getInformation()
-    )
-  }
-})
-for (const ins of instances) {
-	console.log(
-		'🚀 ~ file: app.ts:26 ~ ins.getInformation():',
-		ins.getInformation()
-	)
+function searchAddressHandler(event:Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+  //
 }
+form.addEventListener('submit', searchAddressHandler)
